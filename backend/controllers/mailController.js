7808719bdc2dc1613751b53b3faad1398d80bef8
Jhon Bojrg Soriano <sorianojhonbojrg1234@gmail.com>
2025-09-04 23:37:53 +0800

    MR-07 MR-08 : Added api endpoint getMails and createMail

--- backend/controllers/mailController.js
+++ backend/controllers/mailController.js
@@ -1,10 +1,108 @@
+import { sql } from "../config/db.js";
+
 export const getMails = async (req, res) => {
-    // GET ALL MAIL
+    try {
+        const mails = await sql`
+            SELECT * FROM mails
+            ORDER BY created_at DESC
+        `
+
+        console.log("fetched products", mails);
+        
+        res.status(200).json({
+            success: true,
+            data: mails
+        });
+    } catch (error) {
+        console.log("Error getMails", error);
+        res.status(500).json({ success: false, message: error.message });
+    }
 };
 
+export const getMail = async (req, res) => {
+    // Get Mail   
+}
+
 export const createMail = async (req, res) => {
-    // Create Mail
+    const {
+        reference_no,
+        sender,
+        recipient,
+        mail_type,
+        delivery_mode,
+        courier,
+        quantity,
+        remarks,
+        status,
+        created_at,
+        created_by
+    } = req.body;
+
+    if (
+        !reference_no ||
+        !sender ||
+        !recipient ||
+        !mail_type ||
+        !delivery_mode ||
+        !courier ||
+        !quantity ||
+        !remarks ||
+        !status ||
+        !created_at ||
+        !created_by
+    ) {
+        return res.status(400).json({
+            success: false,
+            message: "All fields are required"
+        });
+    }
+
+    try {
+        const newMail = await sql`
+            INSERT INTO mails (
+                reference_no,
+                sender,
+                recipient,
+                mail_type,
+                delivery_mode,
+                courier,
+                quantity,
+                remarks,
+                status,
+                created_at,
+                created_by
+            )
+            VALUES (
+                ${reference_no},
+                ${sender},
+                ${recipient},
+                ${mail_type},
+                ${delivery_mode},
+                ${courier},
+                ${quantity},
+                ${remarks},
+                ${status},
+                ${created_at},
+                ${created_by}
+            )
+            RETURNING *
+        `
+
+        console.log("New Mail Added: ", newMail);
+        res.status(201).json({ success: true, data: newMail[0] });
+    } catch (error) {
+        console.log("Error createMails", error);
+        res.status(500).json({ success: false, message: error.message });
+    }
 };
+
+export const updateMail = async () => {
+    // Update Mail
+}
+
+export const deleteMail = async () => {
+    // Update Mail
+}
 
 
 // app.get("/api/mails", (req, res) => {
--- backend/routes/mailRoutes.js
+++ backend/routes/mailRoutes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { getMails, createMail } from "../controllers/mailController.js";
+import {
+    getMails,
+    getMail,
+    createMail,
+    updateMail,
+    deleteMail
+} from "../controllers/mailController.js";
 
 const router = express.Router();
 
 router.get("/", getMails);
+router.get("/:id", getMail);
 router.post("/", createMail);
+router.put("/:id", updateMail);
+router.delete("/:id", deleteMail);
 
 export default router;
