b6852cad98c76794d35eed2e54288d305d4f0dca
Jhon Bojrg Soriano <sorianojhonbojrg1234@gmail.com>
2025-09-04 16:58:03 +0800

    MR-05:Setup nodejs and added helmet package

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+//helmet is a security middleware that helps you protect your your app by setting various HTTP headers
+app.use(helmet());
+
+app.get("/test", (req, res) => {
+    console.log(res.getHeaders());
+    res.send("Hello from the backend test");
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000"); 
+});
