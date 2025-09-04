4b947031dae50e962b226bf15fd8763d5de1b5a1
Jhon Bojrg Soriano <sorianojhonbojrg1234@gmail.com>
2025-09-04 17:08:19 +0800

    MR-05: added json,cors,dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
 
 //helmet is a security middleware that helps you protect your your app by setting various HTTP headers
 app.use(helmet());
@@ -11,10 +19,9 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 app.get("/test", (req, res) => {
-    console.log(res.getHeaders());
     res.send("Hello from the backend test");
 })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is running on port 3000"); 
 });
